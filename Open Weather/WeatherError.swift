3d48048ccf5e3d2ab6496c098f118e01de94eda8
nishanttaneja <nishant.taneja2701@gmail.com>
2020-10-14 00:06:36 +0530

    Weather Error Handling

---
+++ Open Weather/WeatherError.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherError.swift
+//  Open Weather
+//
+//  Created by Nishant Taneja on 14/10/20.
+//
+
+import Foundation
+
+enum WeatherError: Error {
+    case APIKeyNotFound
+    case APIURLNotFound
+    case APINotFound
+}
