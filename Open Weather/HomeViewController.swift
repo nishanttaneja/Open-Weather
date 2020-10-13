84dc487571547c417f060c2225ee20fccd340be3
nishanttaneja <nishant.taneja2701@gmail.com>
2020-10-14 00:04:51 +0530

    attach UI to HomeViewController

---
+++ Open Weather/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Open Weather
+//
+//  Created by Nishant Taneja on 13/10/20.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    // IBOutlets
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var highTempLabel: UILabel!
+    @IBOutlet weak var LowTempLabel: UILabel!
+    
+    // IBAction
+    @IBAction func searchButtonTapped(_ sender: UIButton) {}
+}
