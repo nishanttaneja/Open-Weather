b5339e4549eeeb57c7c073c25b53cae4c76ee76e
nishanttaneja <nishant.taneja2701@gmail.com>
2020-10-14 00:05:58 +0530

    create WeatherData Model

---
+++ Open Weather/WeatherData.swift
@@ -0,0 +1,39 @@
+//
+//  WeatherData.swift
+//  Open Weather
+//
+//  Created by Nishant Taneja on 14/10/20.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let weather: [Weather]
+    let main: Main
+    let visibility: Double
+    let wind: Wind
+    let sys: Sys
+    let name: String
+}
+
+// Weather Data Parameters
+struct Weather: Decodable {
+    let id: Int
+    let description: String
+}
+
+struct Main: Decodable {
+    let temp: Double
+    let feels_like: Double
+    let temp_min: Double
+    let temp_max: Double
+}
+
+struct Wind: Decodable {
+    let speed: Double
+    let deg: Double
+}
+
+struct Sys: Decodable {
+    let country: String
+}
