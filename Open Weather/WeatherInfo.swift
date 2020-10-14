0e562e6ab4f3ec5428c1c1208604a998ee9c7d2b
nishanttaneja <nishant.taneja2701@gmail.com>
2020-10-14 22:18:20 +0530

    add more data

--- Open Weather/WeatherInfo.swift
+++ Open Weather/WeatherInfo.swift
@@ -11,4 +11,20 @@ import RealmSwift
 class WeatherInfo: Object {
     @objc dynamic var city = ""
     @objc dynamic var fetchedAt = Date()
+    @objc dynamic var temperature = ""
+    @objc dynamic var feelsLike = ""
+    @objc dynamic var highestTemperature = ""
+    @objc dynamic var lowestTemperature = ""
+    @objc dynamic var condition = ""
+    @objc dynamic var conditionDescription = ""
+    
+    func addParameters(for city: String, _ temperature: String, _ feelsLike: String, _ highestTemperature: String, _ lowestTemperature: String, _ condition: String, conditionDescription: String) {
+        self.city = city
+        self.temperature = temperature
+        self.feelsLike = feelsLike
+        self.highestTemperature = highestTemperature
+        self.lowestTemperature = lowestTemperature
+        self.condition = condition
+        self.conditionDescription = conditionDescription
+    }
 }
