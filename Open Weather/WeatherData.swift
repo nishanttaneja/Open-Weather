2263c1890d67abb5b8178e3a7633649a4b5a50dd
nishanttaneja <nishant.taneja2701@gmail.com>
2020-10-15 01:52:40 +0530

    update weather condition strings

--- Open Weather/WeatherData.swift
+++ Open Weather/WeatherData.swift
@@ -62,21 +62,21 @@ extension WeatherData: Presentable {
     var condition: String {
         switch weather![0].id {
         case 200...232:
-            return "cloud.bolt"
+            return "bolt"
         case 300...321:
-            return "cloud.drizzle"
+            return "drizzle"
         case 500...531:
-            return "cloud.rain"
+            return "rain"
         case 600...622:
-            return "cloud.snow"
+            return "snow"
         case 701...781:
-            return "cloud.fog"
+            return "fog"
         case 800:
-            return "sun.max"
+            return "sun"
         case 801...804:
-            return "cloud.bolt"
+            return "bolt"
         default:
-            return "cloud"
+            return "clouds"
         }
     }
 }
